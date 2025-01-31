22e20f71d5ff059a158174ab327ac0d2add1aebd
meshJohn <meshjohn1@gmail.com>
2025-02-01 00:27:28 +0200

    fix errors

--- components/BookOverview.tsx
+++ components/BookOverview.tsx
@@ -28,9 +28,9 @@ const BookOverview = async ({
     .from(users)
     .where(eq(users.id, userId))
     .limit(1);
-  if (!user) return null;
+
   const borrowingEligibility = {
-    isEligible: availableCopies > 0 && user.status === "APPROVED",
+    isEligible: availableCopies > 0 && user?.status === "APPROVED",
     message:
       availableCopies <= 0
         ? "Book is not available"
@@ -62,11 +62,13 @@ const BookOverview = async ({
           </p>
         </div>
         <p className="book-description">{description}</p>
-        <BorrowBook
-          bookId={id}
-          userId={userId}
-          borrowingEligibility={borrowingEligibility}
-        />
+        {user && (
+          <BorrowBook
+            bookId={id}
+            userId={userId}
+            borrowingEligibility={borrowingEligibility}
+          />
+        )}
       </div>
       <div className="relative flex flex-1 justify-center">
         <div className="relative">
--- components/BorrowBook.tsx
+++ components/BorrowBook.tsx
@@ -39,7 +39,7 @@ const BorrowBook = ({
           title: "Success",
           description: "Book borrowed successfully",
         });
-        router.push("/my-profile");
+        router.push("/");
       }
     } catch (error) {
       toast({
